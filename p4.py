382ed010d670bfdb4d0ceea22722c17ce886c783
katilkorra <katilkorra@gmail.com>
2024-09-02 11:20:46 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,15 @@
+#4.Word shuffle using string manipulation.
+
+words="Taarak Mehta Ka Oolta Chashma"
+
+wordA=words[0:6]
+wordB=words[7:12]
+wordC=words[13:15]
+wordD=words[16:21]
+wordE=words[22:29]
+
+print(wordA, wordB, wordC, wordD, wordE)
+
+print("--- After Changing ---")
+
+print(wordB, wordC, wordE,wordA, wordD )
